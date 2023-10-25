49537fdf1fc10ffe442681dc16e412bcc81d9e40
Ahmad Badar <ahmadbadar145@gmail.com>
2023-10-25 08:53:56 +0700

    Update Car

--- dip/Car.java
+++ dip/Car.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dip;
 
-/**
- *
- * @author Ahmad Badar
- */
-public class Car {
-    
+class Car {
+    private final Engine engine;
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+    void start() {
+        engine.start();
+    }
 }
